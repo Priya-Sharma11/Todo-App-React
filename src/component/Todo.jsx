2a8b6290fe726130793c6834de712fd35d5a4385
priya <ps25283@gmail.com>
2024-02-02 17:51:47 +0530

    todo-app commit

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,17 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import AppName from "./AppName";
 
 const Todo = () => {
+  const[addItems,setAddItems]= useState("");
+  const[addDate,setAddDate]=useState("");
+  const[items,setItem]=useState([]); 
+  const[date,setDate]=useState([]);
+
+const itemEvent=(e)=>{
+  setAddItems(e.target.value);
+}
+
+const dateEvent=(e)=>{
+  setAddDate(e.target.value);
+}
+  const handleOnClick=(event)=>{
+    //console.log(event.target.value);
+    //console.log("Clicked");
+    setItem((oldItems)=>{
+      return [...oldItems,{addItems, addDate}];
+    });
+
+    setAddItems("");
+    setAddDate("");
+    
+  }
   return (
     <>
-      <center class="todo-container">
-        <div class="center_div">
+      <center className="todo-container">
+        <div className="center_div">
           <AppName/>
-          <div class="row">
+          <div className="row">
             <div className="col-4">
-              <input type="text" placeholder="Add Items" />
-              <input type="date" />
-              <button>+</button>
+              <input type="text" 
+              value={addItems}
+              placeholder="Add Items" onChange={itemEvent}/>
+
+              <input type="date" value={addDate} onChange={dateEvent}/>
+
+              <button onClick={handleOnClick}>ADD</button>
+              
+                <ol>
+                 {/*  <li>
+                    {addItems}
+                  </li> */}
+                  {items.map((item,index)=>{
+                    return <li key={index}>
+                      <span>{item.addItems} </span>
+                      <span>{item.addDate}</span>
+
+                    </li>
+                  })}
+                  
+                </ol>
+                
             </div>
           </div>
         </div>
